9a97816884559aafc7666f7d1db595d7eb8232a4
FullMetalFist <m.vilabrera@gmail.com>
2023-02-26 05:23:16 -0500

    create project

---
+++ AgileRobot/AgileRobot/AgileRobotApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgileRobotApp.swift
+//  AgileRobot
+//
+//  Created by Michael Vilabrera on 2/25/23.
+//
+
+import SwiftUI
+
+@main
+struct AgileRobotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ScrumView()
+        }
+    }
+}
---
+++ AgileRobot/AgileRobot/ScrumView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AgileRobot
+//
+//  Created by Michael Vilabrera on 2/25/23.
+//
+
+import SwiftUI
+
+struct ScrumView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ScrumView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumView()
+    }
+}
