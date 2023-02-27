5cf4be544bf38b24cca9ceb10428a4bfc043bd67
FullMetalFist <m.vilabrera@gmail.com>
2023-02-27 12:06:39 -0500

    Creating a card view: Create a daily scrum model

---
+++ AgileRobot/AgileRobot/Models/DailyScrum.swift
@@ -0,0 +1,24 @@
+//
+//  DailyScrum.swift
+//  AgileRobot
+//
+//  Created by Michael Vilabrera on 2/27/23.
+//
+
+import Foundation
+
+struct DailyScrum {
+    var title: String
+    var attendees: [String]
+    var lengthInMinutes: Int
+    var theme: Theme
+}
+
+extension DailyScrum {
+    static let sampleData: [DailyScrum] =
+    [
+        DailyScrum(title: "Design", attendees: ["Cathy", "Daisy", "Simon", "Jony"], lengthInMinutes: 10, theme: .yellow),
+        DailyScrum(title: "App Dev", attendees: ["Katie", "Gary", "Euna", "Luis", "Darla"], lengthInMinutes: 5, theme: .orange),
+        DailyScrum(title: "Web Dev", attendees: ["Chella", "Chris", "Christina", "Eden", "Karla", "Lindsey", "Aga", "Chad", "Jenn", "Sarah"], lengthInMinutes: 5, theme: .poppy)
+    ]
+}
