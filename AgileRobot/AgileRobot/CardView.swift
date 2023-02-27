9e122ab82c64947def446b3f3c806d45ca14a19a
FullMetalFist <m.vilabrera@gmail.com>
2023-02-27 13:29:18 -0500

    Creating a card view: Make the card view accessible

--- AgileRobot/AgileRobot/CardView.swift
+++ AgileRobot/AgileRobot/CardView.swift
@@ -13,11 +13,14 @@ struct CardView: View {
         VStack(alignment: .leading) {
             Text(scrum.title)
                 .font(.headline)
+                .accessibilityAddTraits(.isHeader)
             Spacer()
             HStack {
                 Label("\(scrum.attendees.count)", systemImage: "person.3")
+                    .accessibilityLabel("\(scrum.attendees.count) attendees")
                 Spacer()
                 Label("\(scrum.lengthInMinutes)", systemImage: "clock")
+                    .accessibilityLabel("\(scrum.lengthInMinutes) minute meeting")
                     .labelStyle(.trailingIcon)
             }
             .font(.caption)
@@ -32,7 +35,7 @@ struct CardView_Previews: PreviewProvider {
     static var scrum = DailyScrum.sampleData[0]
     static var previews: some View {
         CardView(scrum: scrum)
-            .background(scrum.theme.mainColor)  // in Preview wasn't working, set to .yellow to notice change
+            .background(.yellow)
             .previewLayout(.fixed(width: 400, height: 60))
     }
 }
