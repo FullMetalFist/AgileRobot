f2a73da15f8ea24ebd664146ff3a3954a5972947
FullMetalFist <m.vilabrera@gmail.com>
2023-02-28 18:41:17 -0500

    Displaying Data in a List: Make scrums identifiable

--- AgileRobot/AgileRobot/AgileRobotApp.swift
+++ AgileRobot/AgileRobot/AgileRobotApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct AgileRobotApp: App {
     var body: some Scene {
         WindowGroup {
-            ScrumView()
+            ScrumsView(scrums: DailyScrum.sampleData)
         }
     }
 }
