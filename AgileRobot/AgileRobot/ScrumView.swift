cae87181ccd5e8270bbaca19f72cc0aebc42d2e5
FullMetalFist <m.vilabrera@gmail.com>
2023-03-08 18:57:09 -0500

    Managing state and life cycle: Extract the meeting footer

--- AgileRobot/AgileRobot/ScrumView.swift
+++ AgileRobot/AgileRobot/ScrumView.swift
@@ -17,32 +17,7 @@ struct ScrumView: View {
             VStack {
                 StandupHeaderView(secondsElapsed: scrumTimer.secondsElapsed, secondsRemaining: scrumTimer.secondsRemaining, theme: standup.theme)
                 Circle().strokeBorder(lineWidth: 24, antialiased: true)
-                HStack {
-                    VStack(alignment: .leading) {
-                        Text("Seconds Elapsed")
-                            .font(.caption)
-                        Label("300", systemImage: "hourglass.bottomhalf.fill")
-                    }
-                    Spacer()
-                    VStack(alignment: .trailing) {
-                        Text("Seconds Remaining")
-                            .font(.caption)
-                        Label("600", systemImage: "hourglass.tophalf.fill")
-                    }
-                }
-                .accessibilityElement(children: .ignore)
-                .accessibilityLabel("Time remaining")
-                .accessibilityValue("10 minutes")
-//                Circle()
-//                    .strokeBorder(lineWidth: 24)
-//                HStack {
-//                    Text("Speaker 1 of 3")
-//                    Spacer()
-//                    Button(action: {}) {
-//                        Image(systemName: "forward.fill")
-//                    }
-//                    .accessibilityLabel("Next speaker")
-//                }
+                StandupFooterView(speakers: scrumTimer.speakers, skipAction: scrumTimer.skipSpeaker)
             }
         }
         .padding()
---
+++ AgileRobot/AgileRobot/StandupFooterView.swift
@@ -0,0 +1,52 @@
+//
+//  StandupFooterView.swift
+//  AgileRobot
+//
+//  Created by Michael Vilabrera on 3/8/23.
+//
+
+import SwiftUI
+
+struct StandupFooterView: View {
+    let speakers: [ScrumTimer.Speaker]
+    var skipAction: () -> Void
+    
+    private var speakerNumber: Int? {
+        guard let index = speakers.firstIndex(where: { !$0.isCompleted }) else { return nil }
+        return index + 1
+    }
+    
+    private var isLastSpeaker: Bool {
+        return speakers.dropLast().reduce(true) { $0 && $1.isCompleted } //example from Apple: speakers.dropLast().allSatisfy { $0.isCompleted }
+    }
+    
+    private var speakerText: String {
+        guard let speakerNumber = speakerNumber else { return "No more speakers" }
+        return "Speaker \(speakerNumber) of \(speakers.count)"
+    }
+    
+    var body: some View {
+        VStack {
+            HStack {
+                if isLastSpeaker {
+                    Text("Last Speaker")
+                } else {
+                    Text(speakerText)
+                    Spacer()
+                    Button(action: skipAction) {
+                        Image(systemName: "forward.fill")
+                    }
+                    .accessibilityLabel("Next speaker")
+                }
+            }
+        }
+        .padding([.bottom, .horizontal])
+    }
+}
+
+struct StandupFooterView_Previews: PreviewProvider {
+    static var previews: some View {
+        StandupFooterView(speakers: DailyScrum.sampleData[0].attendees.speakers, skipAction: {})
+            .previewLayout(.sizeThatFits)
+    }
+}
